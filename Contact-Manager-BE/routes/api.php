08ed6e4da0281bc8670fa3331e07087170395c56
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-06 08:01:39 +0300

    BE- added contact controller

--- Contact-Manager-BE/routes/api.php
+++ Contact-Manager-BE/routes/api.php
@@ -1,17 +1,26 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
 
-Route::group(["middleware" => "auth:api"], function(){    
+
+Route::group(["middleware" => "auth:api"], function(){
+    $user = Auth::user(); 
     Route::group(["prefix" => "user"], function(){
-        Route::get("profile", [AuthController::class, "profile"]);
         Route::post("logout", [AuthController::class, "logout"]);
         Route::post("refresh", [AuthController::class, "refresh"]);
+        Route::post("addContact", [ContactController::class, "addContact"]);
+        Route::get("getAllContacts", [ContactController::class, "getAllContacts"]);
     });
+
 });
+
+//Unauthenticated APIS
 Route::group(["prefix" => "guest"], function(){
+    //catch api for unauthorized users
     Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
+    //login & signup 
     Route::post("login", [AuthController::class, "login"]);
     Route::post("register", [AuthController::class, "register"]);
 });
