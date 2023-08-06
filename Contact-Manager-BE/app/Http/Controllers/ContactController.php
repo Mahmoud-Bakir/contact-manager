8ea9ff0c976b3e4c2153a875eb388d92b7b66159
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-06 08:00:19 +0300

    BE- created contact controller and modal

---
+++ Contact-Manager-BE/app/Http/Controllers/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Auth;
+
+
+class ContactController extends Controller
+{
+    public function addContact(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'number' => 'required|string',
+        ]);
+        $user = Auth::user();
+        $id = $user->id;
+        $contact= new Contact;
+        $contact->name = $request->name;
+        $contact->number= $request->number;
+        $contact->altitude = $request->altitude;
+        $contact->longitude = $request->longitude;
+        $contact->user_id = $id;
+        $contact->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'contact created successfully',
+            'user' => $contact,
+        ]);   
+    }
+
+    public function getAllContacts(){
+        $contacts = Contact::all();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'contacts recieved',
+            'user' => $contacts,
+        ]);   
+    }
+}
